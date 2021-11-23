6d4dfe24b028e5fa2d5b8bc305f5f3fe0847c903
Guruprasad Kamath <guru241987@gmail.com>
2021-11-23 10:45:05 +0530

    Adapt tests to new Contract Structure

--- tests/test_nftickets.py
+++ tests/test_nftickets.py
@@ -6,7 +6,7 @@ def test_nfticket_factory_deploy():
     """
     Test if the contract is correctly deployed.
     """
-    nfticket_factory = deploy_nfticket_factory()
+    nfticket_factory = deploy_nfticket_factory(5)
 
     print(nfticket_factory.address)
     assert nfticket_factory.address != 0x0
@@ -16,9 +16,15 @@ def test_nfticket_factory_mint(accounts):
     """
     Users can mint tickets to their events
     """
-    nfticket_factory = deploy_nfticket_factory()
+    nfticket_factory = deploy_nfticket_factory(5)
     bob = accounts[1]
-    nfticket_factory.createEventTickets(100, 1, "Classical Concert", {"from": bob})
+    ticketInput = [
+        1,
+        100,
+        "Classical Concert",
+        [10, "Regular", "http://image", [False]],
+    ]
+    nfticket_factory.createEventTickets(ticketInput, {"from": bob})
     owner_balance = nfticket_factory.balanceOf(bob, 0)
 
     assert owner_balance == 100
@@ -28,10 +34,16 @@ def test_nfticket_factory_buy(accounts):
     """
     Users can buy tickets from the event owners
     """
-    nfticket_factory = deploy_nfticket_factory()
+    nfticket_factory = deploy_nfticket_factory(5)
     bob = accounts[1]
     alice = accounts[2]
-    nfticket_factory.createEventTickets(100, 1, "Classical Concert", {"from": bob})
+    ticketInput = [
+        1,
+        100,
+        "Classical Concert",
+        [10, "Regular", "http://image", [False]],
+    ]
+    nfticket_factory.createEventTickets(ticketInput, {"from": bob})
 
     nfticket_factory.buyFromOwner(0, 40, {"from": alice, "value": 40})
 
@@ -46,11 +58,17 @@ def test_buy_nfticket_insufficient_funds(accounts):
     """
     Users cannot buy tickets without providing proper funds
     """
-    nfticket_factory = deploy_nfticket_factory()
+    nfticket_factory = deploy_nfticket_factory(5)
 
     bob = accounts[1]
     alice = accounts[2]
-    nfticket_factory.createEventTickets(100, 1, "Classical Concert", {"from": bob})
+    ticketInput = [
+        1,
+        100,
+        "Classical Concert",
+        [10, "Regular", "http://image", [False]],
+    ]
+    nfticket_factory.createEventTickets(ticketInput, {"from": bob})
 
     with brownie.reverts("Not enough funds sent to buy the tickets"):
         nfticket_factory.buyFromOwner(0, 40, {"from": alice, "value": 39})
@@ -60,11 +78,17 @@ def test_buy_insufficient_nftickets(accounts):
     """
     Users cannot buy tickets that are not available
     """
-    nfticket_factory = deploy_nfticket_factory()
+    nfticket_factory = deploy_nfticket_factory(5)
     bob = accounts[1]
     alice = accounts[2]
     jack = accounts[3]
-    nfticket_factory.createEventTickets(100, 1, "Classical Concert", {"from": bob})
+    ticketInput = [
+        1,
+        100,
+        "Classical Concert",
+        [10, "Regular", "http://image", [False]],
+    ]
+    nfticket_factory.createEventTickets(ticketInput, {"from": bob})
 
     nfticket_factory.buyFromOwner(0, 60, {"from": alice, "value": 60})
 
